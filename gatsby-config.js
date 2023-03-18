a0c915c798da8f849a94d74164fb284431b3aec1
rkspice <rkrkspice@gmail.com>
2023-03-18 16:43:08 -0400

    feat: part 7 - dynamic images

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,6 +9,7 @@ module.exports = {
     "gatsby-plugin-image",
     "gatsby-plugin-mdx",
     "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
     {
       resolve: "gatsby-source-filesystem",
       options: {
